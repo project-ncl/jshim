c2d98dc80aee7312dbda928fce636229b6039770
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-03-17 16:48:01 -0400

    Add server support

---
+++ server/src/test/java/org/jboss/pnc/jshim/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.jboss.pnc.jshim;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
